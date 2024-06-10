2da12104504ffcb8e0668307261f7c3c3a047d25
Alberto gutierrez <jes.alberto.ing@gmail.com>
2024-06-10 01:36:07 -0600

    feature:  the form was validated in the controller

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,5 @@
+import { validationResult } from 'express-validator';
+import { Precio, Categoria, Propiedad, Usuario } from '../models/index.js';
 
 const admin = (req, res) => {
     res.render('propiedades/admin', {
@@ -7,14 +9,70 @@ const admin = (req, res) => {
 }
 
 // Formulario para crear un registro de propiedad
-const crear = (req, res) => {
+const crear = async (req, res) => {
+    
+    // Consultar base de datos
+    const [ categorias, precios ] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll()
+    ])
+
     res.render("propiedades/crear", {
       pagina: "Crear Propiedad",
       barra: true,
+      csrfToken: req.csrfToken(),
+      categorias,
+      precios,
+      datos: {},
     });
+}
+
+const guardar = async (req, res) => {
+    // Validacion
+    let resultado = validationResult(req);
+
+    if(!resultado.isEmpty()) {
+        const [ categorias, precios ] = await Promise.all([
+            Categoria.findAll(),
+            Precio.findAll()
+        ]);
+
+        res.render("propiedades/crear", {
+          pagina: "Crear Propiedad",
+          barra: true,
+          csrfToken: req.csrfToken(),
+          categorias,
+          precios,
+          errores: resultado.array(),
+          datos: req.body,
+        });
+    }
+
+    // Crear un registro
+    const { titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio: precioId, categoria: categoriaId } = req.body
+    
+    return
+    try {
+        const propiedadGuardada = await Propiedad.create({
+            titulo,
+            descripcion,
+            habitaciones,
+            estacionamiento,
+            wc,
+            calle,
+            lat,
+            lng,
+            precioId,
+            categoriaId
+        })
+    } catch (err) {
+        console.error(err);
+    }
+    console.log(req.body);
 }
 
 export {
     admin,
     crear,
+    guardar,
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,10 +1,26 @@
 import express from 'express';
-import { admin, crear } from '../controllers/propiedadController.js';
+import { body } from 'express-validator';
+import { admin, crear, guardar } from '../controllers/propiedadController.js';
 
 const router = express.Router();
 
 router.get('/mis-propiedades', admin);
 router.get('/propiedades/crear', crear);
+router.post(
+  "/propiedades/crear",
+  body("titulo").notEmpty().withMessage("El titulo del anuncion es obligado"),
+  body("descripcion")
+    .notEmpty()
+    .withMessage("La descripcion no puede ir vacia")
+    .isLength({ max: 200 }).withMessage('La descripcion es muy larga'),
+  body('categoria').isNumeric().withMessage('Selecciona una catagoria'),
+  body('precio').isNumeric().withMessage('Selecciona un rango de Precios'),
+  body('habitaciones').isNumeric().withMessage('Selecciona la cantidad de habitaciones'),
+  body('estacionamiento').isNumeric().withMessage('Selecciona la cantidad de estacionamientos'),
+  body('wc').isNumeric().withMessage('Selecciona la cantidad de banios'),
+  body('lat').notEmpty().withMessage('Ubica la propiedad en el mapa'),
+  guardar
+);
 
 
 export default router;

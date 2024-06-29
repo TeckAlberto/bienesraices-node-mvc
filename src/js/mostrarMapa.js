a36f1f090fb091b65beacbcd040be219bb6fcc8b
Alberto gutierrez <jes.alberto.ing@gmail.com>
2024-06-28 20:07:57 -0600

    fix: change order the coords

--- src/js/mostrarMapa.js
+++ src/js/mostrarMapa.js
@@ -1,14 +1,18 @@
 (function () {
-  const lat = document.querySelector("#lat").textContent;
-  const lng = document.querySelector("#lng").textContent;
+  const lat = parseFloat(document.querySelector("#lat").textContent);
+  const lng = parseFloat(document.querySelector("#lng").textContent);
   const calle = document.querySelector("#calle").textContent;
-  const mapa = L.map("mapa").setView([lat, lng], 16);
+
+  console.log(lat, lng);
+
+  const mapa = L.map("mapa-propiedad").setView([lat, lng], 14);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(mapa);
+  }).addTo(mapa );
 
   // Agregar el pin
   L.marker([lat, lng]).addTo(mapa).bindPopup(calle);
+
 })();
